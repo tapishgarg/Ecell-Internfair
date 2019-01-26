5206b521f6ed6a1862c35375fa3e50b2f66477d1
joeydash <joydassudipta@gmail.com>
2019-01-26 18:18:38 +0530

    joeydash

--- main/routes/index.js
+++ main/routes/index.js
@@ -1,28 +1,18 @@
 let express = require('express');
 let router = express.Router();
-const exec = require("child_process").exec
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('home/index', {title: 'Express'});
 });
 
-router.get('/tapish', function(req, res, next) {
-  let data = {
-    "name" : "tapishg",
-    "age" : "19"
-  };
-  res.render('tapish', data);
+router.get('/single/tapish', function (req, res, next) {
+    let data = {
+        "name": "tapishg",
+        "age": "19"
+    };
+    res.render('tapish', data);
 });
 
-
-router.get('/reload_app', function(req, res, next) {
-
-  exec("ls", (error, stdout, stderr) => {
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
