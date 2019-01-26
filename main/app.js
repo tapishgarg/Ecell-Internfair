c0993273755a0bb000fe159a63bf7c21e1cccb7b
joeydash <joydassudipta@gmail.com>
2019-01-26 11:50:47 +0530

    example added

--- main/app.js
+++ main/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dbRouter = require('./routes/db');
+
 
 var app = express();
 
--- main/routes/index.js
+++ main/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/tapish', function(req, res, next) {
+  let data = {
+    "name" : "tapishg",
+    "age" : "19"
+  };
+  res.render('tapish', data);
+});
+
+
 module.exports = router;
