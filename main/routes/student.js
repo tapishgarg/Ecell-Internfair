a954b8bebe03d7aef97dae8cd743890615b214f5
joeydash <joydassudipta@gmail.com>
2019-02-23 15:29:47 +0530

    example added

--- main/routes/student.js
+++ main/routes/student.js
@@ -6,5 +6,9 @@ router.get('/profile', function (req, res, next) {
     res.render('student/profile/index');
 });
 
+router.get('/internship/list', function (req, res, next) {
+    res.render('student/internship/index');
+});
+
 
 module.exports = router;
