11d99e437bd1349fdb92fbb2de819a8d5314887d
joeydash <joydassudipta@gmail.com>
2019-03-04 07:19:49 +0530

    example added

--- main/routes/admin.js
+++ main/routes/admin.js
@@ -2,9 +2,17 @@ let express = require('express');
 let router = express.Router();
 
 
-router.get('/startups', function (req, res, next) {
-    res.render('admin/startups/index');
+router.get('/shortlisted_list', function (req, res, next) {
+    res.render('single/shortlisted');
 });
 
+
+router.get('/pending_list', function (req, res, next) {
+    res.render('single/pending');
+});
+
+router.get('/rejected_list', function (req, res, next) {
+    res.render('single/rejected');
+});
 
 module.exports = router;
